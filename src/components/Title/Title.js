a3a962509e7f5104795980ac30fd5900a8ef014b
Katherine Rinas <katierinas@me.com>
2018-03-27 10:59:35 -0600

    new repository with fixed errors

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Title.css";
 
-const Title = props => <h1 className="title">{props.children}<p className="gamename">Friend Memory Tester</p><h2 className="directions">Try to click on each card only once!</h2></h1>
+const Title = props => <h1 className="title">{props.children}<p className="gamename">Friends Memory Tester</p><h2 className="directions">Try to click each card only once!</h2></h1>
 
 
 export default Title;
